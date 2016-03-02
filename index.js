3cd414823aea25e8c6e4ef0bfeff5bf5358144c1
root <root@ip-172-31-20-98.us-west-2.compute.internal>
2016-03-02 20:59:47 +0000

    Add a count

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ var jp = bodyparser.json();
 var up = bodyparser.urlencoded({extended: false});
 
 var words = {};
-
+var submitted = 0;
 
 app.use('/', express.static('./public/submit'));
 app.use('/results', express.static('./public/results'));
 
 app.get('/data', function(req,res) {
-  res.send(JSON.stringify(words));
+  res.send(JSON.stringify({results: words, count: submitted}));
 });
 app.get('/words', function(req, res) {
-  return words;
+  return {result: words, count: submitted};
 });
 
 app.get('/reset',  function(req, res) {
   words = {};
+  submitted = 0;
   res.sendStatus(200);
 });
 app.post('/submit', jp, function(req, res) {
   console.log(req.body);
+  submitted++;
   req.body.forEach(function(word) { 
     word = word.toLowerCase();
     if(word in words) {
